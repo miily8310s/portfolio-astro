0790077f3ae5ac6f472458be46b80e396f093c19
miruoon <rillakerika@gmail.com>
2023-10-25 22:23:04 +0900

    fix: blog/top page

--- src/content/config.ts
+++ src/content/config.ts
@@ -3,15 +3,16 @@ import { z, defineCollection } from "astro:content";
 const blog = defineCollection({
   type: "content",
   schema: z.object({
-    layout: z.any(),
     title: z.string(),
     pubDate: z.date(),
     description: z.string(),
     author: z.string(),
-    image: z.object({
-      url: z.string(),
-      alt: z.string(),
-    }),
+    image: z
+      .object({
+        url: z.string(),
+        alt: z.string(),
+      })
+      .optional(),
     tags: z.array(z.string()),
   }),
 });
