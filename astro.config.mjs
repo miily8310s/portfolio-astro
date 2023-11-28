89b5fca8ae7e34007100d11d4e96fef525787bca
miruoon <rillakerika@gmail.com>
2023-11-28 22:04:19 +0900

    chore: update astro

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,20 @@
 import { defineConfig } from "astro/config";
-
 import tailwind from "@astrojs/tailwind";
+import mdx from "@astrojs/mdx";
+import astroExpressiveCode from "astro-expressive-code";
+
+/** @type {import('astro-expressive-code').AstroExpressiveCodeOptions} */
+const astroExpressiveCodeOptions = {
+  // Example: Change the themes
+  themes: "dracula",
+};
 
 // https://astro.build/config
 export default defineConfig({
   site: "https://miruo-katanugu.com",
-  integrations: [tailwind()],
+  integrations: [
+    tailwind(),
+    astroExpressiveCode(astroExpressiveCodeOptions),
+    mdx(),
+  ],
 });
