62c0df66d3fe329a17cb96635245a4bf73a2a8d0
miruoon <rillakerika@gmail.com>
2025-03-08 11:44:19 +0900

    expressiveCode update to 0.4

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,16 +3,15 @@ import tailwindcss from "@tailwindcss/vite";
 import mdx from "@astrojs/mdx";
 import astroExpressiveCode from "astro-expressive-code";
 
-/** @type {import('astro-expressive-code').AstroExpressiveCodeOptions} */
-const astroExpressiveCodeOptions = {
-  // Example: Change the themes
-  themes: "dracula",
-};
-
 // https://astro.build/config
 export default defineConfig({
   site: "https://miruo-katanugu.com",
-  integrations: [astroExpressiveCode(astroExpressiveCodeOptions), mdx()],
+  integrations: [
+    astroExpressiveCode({
+      themes: ["dracula", "dracula-soft"],
+    }),
+    mdx(),
+  ],
   vite: {
     plugins: [tailwindcss()],
   },
