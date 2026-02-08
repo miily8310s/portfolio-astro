70ce1afac3c0348b47f49354d43d2f75a2f93906
miruoo <rillakerika@gmail.com>
2026-02-08 12:53:52 +0900

    Fix/ogp rss improvement (#11)
    * fix: OGP画像を更新&rssを最新順にソート
    
    * fix: デフォルトogp画像とtoggleの修正

--- src/pages/rss.xml.ts
+++ src/pages/rss.xml.ts
@@ -4,11 +4,14 @@ import type { APIContext } from "astro";
 
 export async function GET(context: APIContext) {
   const blog = await getCollection("blog");
+  const sortedPosts = blog.sort(
+    (a, b) => b.data.pubDate.valueOf() - a.data.pubDate.valueOf()
+  );
   return rss({
     title: "miruoo's Blog",
     description: "miruoo's Blog is Blog site about Web/Mobile Development tips",
     site: context.site?.origin ?? "",
-    items: blog.map((post) => ({
+    items: sortedPosts.map((post) => ({
       title: post.data.title,
       pubDate: post.data.pubDate,
       description: post.data.description,
